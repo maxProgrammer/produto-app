4bda72ec9ab3d7e9422a210ff6029762237649cc
maxProgrammer <maxwneto.poo@gmail.com>
2022-05-07 11:25:44 -0300

    criado rotas findById,PUT e delete

--- src/main/java/com/api/produtoapp/controllers/ProdutoController.java
+++ src/main/java/com/api/produtoapp/controllers/ProdutoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value="/api")
@@ -19,17 +20,49 @@ public class ProdutoController {
     @Autowired
     ProdutoService produtoService;
 
-    @PostMapping("cadastrar-produto")
-    public ResponseEntity<ProdutoModel> cadastrarProduto(@RequestBody @Valid ProdutoDto produtoDto){
+    @PostMapping("addProduct")
+    public ResponseEntity<Object> addProduct(@RequestBody @Valid ProdutoDto produtoDto){
         ProdutoModel produtoModel = new ProdutoModel();
         BeanUtils.copyProperties(produtoDto,produtoModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(produtoService.save(produtoModel));
     }
 
-    @GetMapping("/produtos")
+    @GetMapping("/allProducts")
     public ResponseEntity<List<ProdutoModel>> getAllProducts(){
         return ResponseEntity.status(HttpStatus.OK).body(produtoService.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneProdduct(@PathVariable(value = "id") Long id){
+        Optional<ProdutoModel> produtoModelOptional = produtoService.findById(id);
+        if(!produtoModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(produtoModelOptional.get());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable(value= "id")  Long id, @RequestBody @Valid ProdutoDto produtoDto){
+        Optional<ProdutoModel> produtoModelOptional = produtoService.findById(id);
+        if(!produtoModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        ProdutoModel produtoModel = new ProdutoModel();
+        BeanUtils.copyProperties(produtoDto,produtoModel);
+        produtoModel.setId(produtoModelOptional.get().getId());
+        return ResponseEntity.status(HttpStatus.OK).body(produtoService.save(produtoModel));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object>  delProduct(@PathVariable(value = "id")Long id){
+        Optional<ProdutoModel> produtoModelOptional = produtoService.findById(id);
+        if(!produtoModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        produtoService.delete(produtoModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Parsing Product deleted Successfully");
+    }
+
 
 
 }
