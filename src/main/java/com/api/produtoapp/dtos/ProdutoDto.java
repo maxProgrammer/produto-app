b5dd5b44ab7d043e48623a57fe1c6c2ac0fd24d9
maxProgrammer <maxwneto.poo@gmail.com>
2022-05-07 11:57:17 -0300

    alterado anotação String DTO para @NotBlank

--- src/main/java/com/api/produtoapp/dtos/ProdutoDto.java
+++ src/main/java/com/api/produtoapp/dtos/ProdutoDto.java
@@ -2,12 +2,13 @@ package com.api.produtoapp.dtos;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
 public class ProdutoDto {
 
-    @NotNull
+    @NotBlank
     private String nome;
     @NotNull
     private long quantidade;
